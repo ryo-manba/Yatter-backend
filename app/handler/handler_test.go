9fdccc0820c60c9ee774963abe76fc675fb7c960
Ryo <matsukawa.ryo42@gmail.com>
2023-03-23 22:33:23 +0900

    add: テーブル駆動を導入した

--- app/handler/handler_test.go
+++ app/handler/handler_test.go
@@ -13,51 +13,63 @@ import (
 	"yatter-backend-go/app/app"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAccountRegistration(t *testing.T) {
 	c := setup(t)
 	defer c.Close()
 
-	func() {
-		resp, err := c.PostJSON("/v1/accounts", `{"username":"john"}`)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !assert.Equal(t, resp.StatusCode, http.StatusOK) {
-			return
-		}
+	testCases := []struct {
+		name        string
+		method      string
+		apiPath     string
+		payload     string
+		expectedRes map[string]interface{}
+	}{
+		{
+			name:    "RegisterAccount",
+			method:  "POST",
+			apiPath: "/v1/accounts",
+			payload: `{"username":"john"}`,
+			expectedRes: map[string]interface{}{
+				"username": "john",
+			},
+		},
+		{
+			name:    "GetAccount",
+			method:  "GET",
+			apiPath: "/v1/accounts/john",
+			expectedRes: map[string]interface{}{
+				"username": "john",
+			},
+		},
+	}
 
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Fatal(err)
-		}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			var resp *http.Response
+			var err error
 
-		var res map[string]interface{}
-		if assert.NoError(t, json.Unmarshal(body, &res)) {
-			assert.Equal(t, "john", res["username"])
-		}
-	}()
+			if tc.method == "POST" {
+				resp, err = c.PostJSON(tc.apiPath, tc.payload)
+			} else if tc.method == "GET" {
+				resp, err = c.Get(tc.apiPath)
+			}
 
-	func() {
-		resp, err := c.Get("/v1/accounts/john")
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !assert.Equal(t, resp.StatusCode, http.StatusOK) {
-			return
-		}
+			require.NoError(t, err)
+			require.Equal(t, http.StatusOK, resp.StatusCode)
 
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Fatal(err)
-		}
+			body, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
 
-		var res map[string]interface{}
-		if assert.NoError(t, json.Unmarshal(body, &res)) {
-			assert.Equal(t, "john", res["username"])
-		}
-	}()
+			var res map[string]interface{}
+			require.NoError(t, json.Unmarshal(body, &res))
+
+			assert.Equal(t, tc.expectedRes["username"], res["username"])
+		})
+	}
 }
 
 func setup(t *testing.T) *C {
