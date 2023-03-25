84b870db3c463cc1c21ebcf21659b79a3ab22350
Ryo <matsukawa.ryo42@gmail.com>
2023-03-25 18:42:39 +0900

    add: statusハンドラーのテストを追加

--- app/handler/handler_test.go
+++ app/handler/handler_test.go
@@ -9,12 +9,12 @@ import (
 	"net/url"
 	"path"
 	"testing"
-
 	"yatter-backend-go/app/app"
 
 	"github.com/stretchr/testify/assert"
 )
 
+/// Accounts
 func TestAccountRegistration(t *testing.T) {
 	c := setup(t)
 	defer c.Close()
@@ -41,7 +41,7 @@ func TestAccountRegistration(t *testing.T) {
 			expectedRes:  map[string]interface{}{},
 		},
 		{
-			name:         "異常系：パラメータが不正",
+			name:         "異常系：不正なパラメータ",
 			payload:      `{"name":"john"}`,
 			expectedCode: http.StatusBadRequest,
 			expectedRes:  map[string]interface{}{},
@@ -71,7 +71,7 @@ func TestAccountRegistration(t *testing.T) {
 	}
 }
 
-func TestAccountGet(t *testing.T) {
+func TestAccountsGet(t *testing.T) {
 	c := setup(t)
 	defer c.Close()
 
@@ -127,6 +127,130 @@ func TestAccountGet(t *testing.T) {
 	}
 }
 
+/// status
+func TestStatusesPost(t *testing.T) {
+	c := setup(t)
+	defer c.Close()
+
+	const apiPath = "/v1/statuses"
+	testCases := []struct {
+		name         string
+		username     string
+		payload      string
+		expectedCode int
+		expectedRes  map[string]interface{}
+	}{
+		{
+			name:         "正常系：ステータスを作成できる",
+			username:     "test-user1",
+			payload:      `{"status": "ピタ ゴラ スイッチ♪", "media_ids": [0]}`,
+			expectedCode: http.StatusOK,
+			expectedRes: map[string]interface{}{
+				"content": "ピタ ゴラ スイッチ♪",
+			},
+		},
+		{
+			name:         "異常系：認証できない",
+			username:     "",
+			payload:      `{"content": "ピタ ゴラ スイッチ♪", "media_ids": [0]}`,
+			expectedCode: http.StatusUnauthorized,
+			expectedRes:  map[string]interface{}{},
+		},
+		{
+			name:         "異常系：不正なパラメータ",
+			username:     "test-user1",
+			payload:      `{"content":"ピタ ゴラ スイッチ♪", , "media_ids": [0]}`,
+			expectedCode: http.StatusBadRequest,
+			expectedRes:  map[string]interface{}{},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			var resp *http.Response
+			var err error
+
+			// 認証する
+			resp, err = c.PostJSONWithAuth(apiPath, tc.payload, tc.username)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expectedCode, resp.StatusCode)
+
+			body, err := io.ReadAll(resp.Body)
+			assert.NoError(t, err)
+
+			if tc.expectedCode == http.StatusOK {
+				var res map[string]interface{}
+				assert.NoError(t, json.Unmarshal(body, &res))
+				assert.Equal(t, tc.expectedRes["content"], res["content"])
+			}
+		})
+	}
+}
+
+func TestStatusesGet(t *testing.T) {
+	c := setup(t)
+	defer c.Close()
+
+	const apiPath = "/v1/statuses/"
+	testCases := []struct {
+		name         string
+		pathParam    string
+		expectedCode int
+		expectedRes  map[string]interface{}
+	}{
+		{
+			name:         "正常系：ステータスが存在する",
+			pathParam:    "1",
+			expectedCode: http.StatusOK,
+			expectedRes: map[string]interface{}{
+				"content": "Test content for user 1",
+			},
+		},
+		{
+			name:         "正常系：ステータスが存在しない",
+			pathParam:    "10000",
+			expectedCode: http.StatusOK,
+			expectedRes:  map[string]interface{}{},
+		},
+		{
+			name:         "異常系：パラメータが数値ではない",
+			pathParam:    "hoge",
+			expectedCode: http.StatusBadRequest,
+			expectedRes:  map[string]interface{}{},
+		},
+		{
+			name:         "異常系：パラメータなし",
+			pathParam:    "",
+			expectedCode: http.StatusUnauthorized,
+			expectedRes:  map[string]interface{}{},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			var resp *http.Response
+			var err error
+
+			resp, err = c.Get(apiPath + tc.pathParam)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expectedCode, resp.StatusCode)
+
+			body, err := io.ReadAll(resp.Body)
+			assert.NoError(t, err)
+
+			if tc.expectedCode == http.StatusOK {
+				var res map[string]interface{}
+				assert.NoError(t, json.Unmarshal(body, &res))
+				assert.Equal(t, tc.expectedRes["content"], res["content"])
+			}
+		})
+	}
+}
+
 func setup(t *testing.T) *C {
 	app, err := app.NewTestApp()
 	if err != nil {
@@ -170,4 +294,14 @@ func (c *C) asURL(apiPath string) string {
 	baseURL, _ := url.Parse(c.Server.URL)
 	baseURL.Path = path.Join(baseURL.Path, apiPath)
 	return baseURL.String()
+}
+
+func (c *C) PostJSONWithAuth(apiPath string, payload string, username string) (*http.Response, error) {
+	req, err := http.NewRequest("POST", c.asURL(apiPath), bytes.NewReader([]byte(payload)))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authentication", "username "+username)
+	return c.Server.Client().Do(req)
 }
